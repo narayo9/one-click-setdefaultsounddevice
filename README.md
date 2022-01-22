9b913179deabd1c847b6bc864ca2154d496ff7bb
Chanuk Kim <chanuk.kim@devsisters.com>
2022-01-22 18:44:53 +0900

    DONE

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-one-click-setdefaultsounddevice
+# windows one button setsounddevices
+
+dependency: [nircmd](https://www.nirsoft.net/utils/nircmd.html)
---
+++ changeDevice.cmd
@@ -0,0 +1,4 @@
+nircmd.exe setdefaultsounddevice "%1"
+nircmd.exe setdefaultsounddevice "%1 mic"
+nircmd.exe setdefaultsounddevice "%1" 2
+nircmd.exe setdefaultsounddevice "%1 mic" 2
